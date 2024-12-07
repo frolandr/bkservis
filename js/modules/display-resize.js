7dbe885ebe4986249608ada5232713dc8fc0a69c
frolandr <sevost@yandex.ru>
2024-12-07 21:09:14 +0400

    удал

--- js/modules/display-resize.js
+++
@@ -1,8 +0,0 @@
-/*================= Реакция на изменение размера дисплея ====================*/
-// window.addEventListener('resize', function() {
-//     /* Закрытие мобильного меню при clientWidth > 767px ---------------------*/
-//     if (document.documentElement.clientWidth > 767) {
-//         document.querySelector('input[type="checkbox"]').checked = false;
-//     }
-// });
-/*===========================================================================*/
